71a157ac9947346193b10ead378539284a574644
zhengzhanghai <zzh_xiaohai@163.com>
2020-05-27 18:30:32 +0800

    add project

---
+++ OCDeepKnowledge/OCDeepKnowledge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCDeepKnowledge
+//
+//  Created by 郑章海 on 2020/5/27.
+//  Copyright © 2020 zzh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
