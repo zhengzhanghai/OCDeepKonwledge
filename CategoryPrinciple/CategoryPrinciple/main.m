7582c7326f007a3203f8a9a3947ad0bcb8bc76e6
zhengzhanghai <zzh_xiaohai@163.com>
2020-07-03 18:33:36 +0800

    add get function list

--- CategoryPrinciple/CategoryPrinciple/main.m
+++ CategoryPrinciple/CategoryPrinciple/main.m
@@ -10,6 +10,31 @@
 #import "Person.h"
 #import "Person+Sleep.h"
 #import "Person+Eat.h"
+#import <objc/message.h>
+
+/* 获取对象的所有方法 */
+NSArray * getAllMethods(Class class)
+{
+    unsigned int methodCount =0;
+    Method* methodList = class_copyMethodList(class, &methodCount);
+    NSMutableArray *methodsArray = [NSMutableArray arrayWithCapacity:methodCount];
+    
+    for(int i=0;i<methodCount;i++)
+    {
+        Method temp = methodList[i];
+        IMP imp = method_getImplementation(temp);
+        SEL name_f = method_getName(temp);
+        const char* name_s =sel_getName(method_getName(temp));
+        int arguments = method_getNumberOfArguments(temp);
+        const char* encoding =method_getTypeEncoding(temp);
+        NSLog(@"方法名：%@,参数个数：%d,编码方式：%@",[NSString stringWithUTF8String:name_s],
+              arguments,
+              [NSString stringWithUTF8String:encoding]);
+        [methodsArray addObject:[NSString stringWithUTF8String:name_s]];
+    }
+    free(methodList);
+    return methodsArray;
+}
 
 
 int main(int argc, const char * argv[]) {
